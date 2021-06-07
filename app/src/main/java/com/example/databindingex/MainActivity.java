8acf7fc2e740fa414627379e6b30139f2de99c13
dits1019 <jdits1019@gmail.com>
2021-06-07 17:46:08 +0900

    뷰 모델 필드를 뷰에 연동

--- app/src/main/java/com/example/databindingex/MainActivity.java
+++ app/src/main/java/com/example/databindingex/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.databindingex;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ObservableField;
 
 import android.os.Bundle;
 
+import com.example.databindingex.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private ViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // 언더바(_)가 들어간 표현 방식은 스네이크 케이스(ex. activity_main.xml)
+        // 첫 글자와 중간 글자들이 대문자일 경우는 파스칼 케이스(ex. MainActivity.java)
+
+        // 데이터 바인딩을 사용할 때에는 다른 형태로 뷰를 생성
+        // 자동 생성된 객체의 이름은 파스칼 케이스에 Binding이 붙은 형태
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        // 레이아웃에 변수와 연결
+        viewModel = new ViewModel();
+        binding.setViewModel(viewModel);
+
+        // 뷰 모델의 값을 설정
+        viewModel.title.set("Hello World");
     }
+
+    // ObservableField는 값이 바뀌면 연동된 DataBinding에게 정보를 전달하고 이를 이용해 UI를 업데이트할 수 있음
+    public static class ViewModel{
+        public ObservableField<String> title = new ObservableField<>();
+    }
+
 }
